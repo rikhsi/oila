0fe7ab3a65e253903a6fb3b3ab18a863482422d6
Rikhsi <omonbek.rikhsimboev@eventplatform.ru>
2024-09-09 15:53:24 +0500

    fix

--- apps/management/module-federation.config.ts
+++ apps/management/module-federation.config.ts
@@ -8,14 +8,10 @@ const config: ModuleFederationConfig = {
   additionalShared: [
     {
       libraryName: '@angular/cdk',
-      sharedConfig: { singleton: true, strictVersion: true },
-    },
-    {
-      libraryName: '@angular/common/http',
       sharedConfig: { singleton: true, eager: true },
     },
     {
-      libraryName: '@angular/core/rxjs-interop',
+      libraryName: '@angular/animations',
       sharedConfig: { singleton: true, eager: true },
     },
     {
@@ -23,19 +19,11 @@ const config: ModuleFederationConfig = {
       sharedConfig: { singleton: true, eager: true, requiredVersion: false },
     },
     {
-      libraryName: '@angular/core',
-      sharedConfig: { singleton: true, eager: true, requiredVersion: false },
-    },
-    {
-      libraryName: '@angular/animations',
+      libraryName: '@angular/common/http',
       sharedConfig: { singleton: true, eager: true },
     },
     {
-      libraryName: '@angular/animations',
-      sharedConfig: { singleton: true, eager: true },
-    },
-    {
-      libraryName: '@angular/router',
+      libraryName: 'gridstack/dist/angular',
       sharedConfig: { singleton: true, eager: true },
     },
     {
@@ -43,19 +31,27 @@ const config: ModuleFederationConfig = {
       sharedConfig: { singleton: true, eager: true },
     },
     {
-      libraryName: '@jsverse/transloco',
-      sharedConfig: { singleton: true, eager: true },
-    },
-    {
-      libraryName: 'ng-zorro-antd',
-      sharedConfig: { singleton: true, eager: true },
-    },
-    {
-      libraryName: 'ngx-mask',
+      libraryName: '@angular/router',
       sharedConfig: { singleton: true, eager: true },
     },
     {
       libraryName: 'ngx-progressbar',
+      sharedConfig: { singleton: true, eager: true },
+    },
+    {
+      libraryName: '@angular/core',
+      sharedConfig: { singleton: true, eager: true, requiredVersion: false },
+    },
+    {
+      libraryName: '@angular/core/rxjs-interop',
+      sharedConfig: { singleton: true, eager: true },
+    },
+    {
+      libraryName: '@jsverse/transloco',
+      sharedConfig: { singleton: true, eager: true },
+    },
+    {
+      libraryName: '@ng-zorro-antd',
       sharedConfig: { singleton: true, eager: true },
     },
     {
@@ -64,6 +60,10 @@ const config: ModuleFederationConfig = {
     },
     {
       libraryName: '@auth0/angular-jwt',
+      sharedConfig: { singleton: true, eager: true },
+    },
+    {
+      libraryName: '@ant-design/icons-angular',
       sharedConfig: { singleton: true, eager: true },
     },
   ],
--- apps/management/src/app/remote-entry/entry.component.ts
+++ apps/management/src/app/remote-entry/entry.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   selector: 'mg-management-entry',
   template: `<router-outlet></router-outlet>`,
 })
